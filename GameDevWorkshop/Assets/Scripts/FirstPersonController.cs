94cc7c6bc33a6e5e11fd283ed1696a4df1711877
Bowditch <bowditch@ohio.edu>
2023-09-14 10:53:37 -0400

    Class 2023.09.14 in-class

--- GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
@@ -6,6 +6,7 @@ Script for basic first person character and camera controls
 
 
 //Libraries that we're using
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -36,27 +37,81 @@ public class FirstPersonController : MonoBehaviour
     void Start()
     {
 
+        characterController = GetComponent<CharacterController>();
+
+        //Hides the mouse cursor at start
+        Cursor.lockState = CursorLockMode.Locked;
+
     }
 
 
         // Update is called once per frame
     void Update()
     {
-        
+        grounded = characterController.isGrounded;
+        MovePlayer();
+        Look();
+    }
+
+    public void MovePlayer()
+    {
+        //Direction to move
+        Vector3 moveVec = transform.right * moveInput.x + transform.forward * moveInput.y;
+
+        //Move Controller
+        characterController.Move(moveVec * speed * Time.deltaTime);
+
+        //Add Gravity
+        playerVelocity.y += gravity * Time.deltaTime;
+
+        if (grounded && playerVelocity.y < 0) {
+
+            playerVelocity.y = -2.5f;
+        }
+
+        characterController.Move(playerVelocity * Time.deltaTime);
+    }
+
+    public void Look()
+    {
+        float xAmount = mouseMovement.x * sensitivity * Time.deltaTime;
+        float yAmount = mouseMovement.y * sensitivity * Time.deltaTime;
+
+        transform.Rotate(Vector3.up * mouseMovement * sensitivity * Time.deltaTime);
+
+        cam_x_rotation -= xAmount;
+        cam_x_rotation = Mathf.Clamp(cam_x_rotation, -90f, 90f);
+
     }
 
     public void OnMove(InputAction.CallbackContext context) 
     { 
-    
+
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Input Value: " + moveInput.ToString());
+        
     }
 
     public void OnLook(InputAction.CallbackContext context)
     {
-            
+        mouseMovement = context.ReadValue<Vector2>();
+        Debug.Log("Mouse Movement: " + mouseMovement.ToString());
+
     }
 
     public void OnJump(InputAction.CallbackContext context)
     {
 
+        Jump();
+        Debug.Log("Look ma, I jumped");
     }
+
+    public void Jump()
+    {
+        if (grounded)
+        {
+            playerVelocity.y = jumpForce;
+        }
+    }
+
 }
