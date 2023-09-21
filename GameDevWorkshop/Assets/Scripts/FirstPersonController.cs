f24acc62ce9b0ac1674ac4bfab7b52bb4915c979
Bowditch <bowditch@ohio.edu>
2023-09-21 10:51:40 -0400

    Call 2023.09.21 class updates

--- GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
+++ GameDevWorkshop/Assets/Scripts/FirstPersonController.cs
@@ -16,9 +16,9 @@ public class FirstPersonController : MonoBehaviour
 {
 
     //Player Variables
-    public float speed = 2.0f;
+    public float speed = 15.0f;
     public float gravity = -10.0f;
-    public float jumpForce = 2.0f;
+    public float jumpForce = 4.0f;
 
     //Movement and Looking Variables
     private CharacterController characterController;
@@ -33,6 +33,30 @@ public class FirstPersonController : MonoBehaviour
     private float cam_x_rotation;
 
 
+    
+
+    public void OnMove(InputAction.CallbackContext context)
+    {
+
+        moveInput = context.ReadValue<Vector2>();
+        Debug.Log("Move Input Value: " + moveInput.ToString());
+
+    }
+
+    public void OnLook(InputAction.CallbackContext context)
+    {
+        mouseMovement = context.ReadValue<Vector2>();
+        Debug.Log("Mouse Movement: " + mouseMovement.ToString());
+
+    }
+
+    public void OnJump(InputAction.CallbackContext context)
+    {
+
+        Jump();
+        Debug.Log("Look ma, I jumped");
+    }
+
     // Start is called before the first frame update
     void Start()
     {
@@ -45,14 +69,13 @@ public class FirstPersonController : MonoBehaviour
     }
 
 
-        // Update is called once per frame
+    // Update is called once per frame
     void Update()
     {
         grounded = characterController.isGrounded;
         MovePlayer();
         Look();
     }
-
     public void MovePlayer()
     {
         //Direction to move
@@ -74,37 +97,21 @@ public class FirstPersonController : MonoBehaviour
 
     public void Look()
     {
-        float xAmount = mouseMovement.x * sensitivity * Time.deltaTime;
-        float yAmount = mouseMovement.y * sensitivity * Time.deltaTime;
+        float xAmount = mouseMovement.y * sensitivity * Time.deltaTime;
+        float yAmount = mouseMovement.x * sensitivity * Time.deltaTime;
 
-        transform.Rotate(Vector3.up * mouseMovement * sensitivity * Time.deltaTime);
+        transform.Rotate(Vector3.up * mouseMovement.x * sensitivity * Time.deltaTime);
 
         cam_x_rotation -= xAmount;
         cam_x_rotation = Mathf.Clamp(cam_x_rotation, -90f, 90f);
 
-    }
+        //Sets camera's local rotation. Player will be able to look up and down 
 
-    public void OnMove(InputAction.CallbackContext context) 
-    { 
-
-        moveInput = context.ReadValue<Vector2>();
-        Debug.Log("Move Input Value: " + moveInput.ToString());
-        
-    }
-
-    public void OnLook(InputAction.CallbackContext context)
-    {
-        mouseMovement = context.ReadValue<Vector2>();
-        Debug.Log("Mouse Movement: " + mouseMovement.ToString());
+        cameraLive.transform.localRotation = Quaternion.Euler(cam_x_rotation, 0, 0);
 
     }
 
-    public void OnJump(InputAction.CallbackContext context)
-    {
 
-        Jump();
-        Debug.Log("Look ma, I jumped");
-    }
 
     public void Jump()
     {
