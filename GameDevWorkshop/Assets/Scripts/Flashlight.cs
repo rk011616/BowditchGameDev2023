26508fae2a943632460e3cc6f83ef9f5d9fb1737
John Bowditch <john.bowditch@me.com>
2023-10-16 11:52:36 -0400

    Updates to Flashlight.cs from class 2023.10.12

--- GameDevWorkshop/Assets/Scripts/Flashlight.cs
+++ GameDevWorkshop/Assets/Scripts/Flashlight.cs
@@ -8,6 +8,8 @@ public class Flashlight : MonoBehaviour
 
     public Light light;
 
+    public AudioSource audioSource;
+
 
     // Start is called before the first frame update
     void Start()
@@ -23,7 +25,24 @@ public class Flashlight : MonoBehaviour
 
     public void OnClick(InputAction.CallbackContext context)
     {
-        light.enabled = !light.enabled;
+
+        if(context.phase == InputActionPhase.Started)
+        {
+
+       
+        //Simple Enable/Disable statement
+        //light.enabled = !light.enabled;
+
+        //Random Color Range selector
+        float r = Random.Range(0.0f, 1.0f);
+        float g = Random.Range(0.0f, 1.0f);
+        float b = Random.Range(0.0f, 1.0f);
+        light.color = new Color(r, g, b);
+
+        audioSource.pitch = Random.Range(0.85f, 1.15f);
+        audioSource.Play();
+
+        }
     }
 
 }
