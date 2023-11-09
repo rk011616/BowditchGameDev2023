4be472e0f43dc6363e84a4682579d6f6e73af3a6
Bowditch <bowditch@ohio.edu>
2023-11-09 10:48:26 -0500

    In-class additions 2023.11.09

--- Asteroids/Assets/Asteroids/Asteroid.cs
+++ Asteroids/Assets/Asteroids/Asteroid.cs
@@ -10,11 +10,14 @@ public class Asteroid : MonoBehaviour
     public float move_speed;
     private Rigidbody rb;
 
+    [HideInInspector] public GameObject spawner;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody>();
+        Vector3 dir = (Vector3.zero - transform.position).normalized;
+        rb.AddForce(dir * move_speed, ForceMode.Impulse);
     }
 
     // Update is called once per frame
@@ -33,6 +36,8 @@ public class Asteroid : MonoBehaviour
             if (life <= 0)
             {
                 Destroy(gameObject);
+                spawner.GetComponent<Spawner>().asteroids_count--;
+                spawner.GetComponent<Spawner>().CheckAsteroidCount();
             }
         }
     }
